e5b53879c78a006171fc6280b9639ce1dd087931
FrankyFour <61128478+FrankyFour@users.noreply.github.com>
2020-02-26 16:30:28 +1000

    first commit

---
+++ precache-manifest.04a428285e5204d18ff3cb09661ea0c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e0e11c7c1879a1219293e53b75c4c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb479cd541c5399401c9",
+    "url": "/static/css/main.0511349c.chunk.css"
+  },
+  {
+    "revision": "505e2333f3d1b464b8ea",
+    "url": "/static/js/2.f0bac6a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f0bac6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb479cd541c5399401c9",
+    "url": "/static/js/main.780a4608.chunk.js"
+  },
+  {
+    "revision": "acd6e3fd0a2b39447943",
+    "url": "/static/js/runtime-main.d763aab6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
